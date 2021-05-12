faa014340409e8735f82113a0e6953b7544fcd0d
CCH0124 <cchong0124@gmail.com>
2021-05-12 13:34:12 +0800

    add hellocontroller

---
+++ src/main/java/cch/com/example/skaffold/controller/HelloController.java
@@ -0,0 +1,22 @@
+package cch.com.example.skaffold.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        StringBuilder message = new StringBuilder("Hello Skaffold!");
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            message.append(" From host: " + ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return message.toString();
+    }
+}
