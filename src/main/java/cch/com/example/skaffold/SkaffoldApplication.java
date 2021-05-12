b1697d1d726807f08fab3b8a9df406b3928bd605
CCH0124 <cchong0124@gmail.com>
2021-05-12 13:29:29 +0800

    init

---
+++ src/main/java/cch/com/example/skaffold/SkaffoldApplication.java
@@ -0,0 +1,13 @@
+package cch.com.example.skaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaffoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cch/com/example/skaffold/SkaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package cch.com.example.skaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
