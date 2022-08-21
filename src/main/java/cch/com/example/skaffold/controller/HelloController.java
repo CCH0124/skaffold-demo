3411c2fbaf015ce385ad3188463fe17a083ccce8
CCH0124 <cchong0124@gmail.com>
2022-08-21 10:48:49 +0800

    add api interface and fix pom file

--- src/main/java/cch/com/example/skaffold/controller/HelloController.java
+++ src/main/java/cch/com/example/skaffold/controller/HelloController.java
@@ -2,12 +2,18 @@ package cch.com.example.skaffold.controller;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.logging.Logger;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger LOGGER = Logger.getLogger(HelloController.class.getName());
+
     @GetMapping("/hello")
     public String hello() {
         StringBuilder message = new StringBuilder("Hello Skaffold!");
@@ -18,5 +24,11 @@ public class HelloController {
             e.printStackTrace();
         }
         return message.toString();
+    }
+
+    @GetMapping("/states")
+    public ResponseEntity<?> states() {
+        LOGGER.info("Get Itachi");
+        return ResponseEntity.ok(Map.of("name", "itachi", "age", 28));
     }
 }
